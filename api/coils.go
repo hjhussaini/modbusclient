d2d98e34dab8cd1983c5f57b0457d1fb569a6b28
Hamed Haji Hussaini <hajihussaini@afrabon.com>
2020-10-21 21:02:02 +0330

    api.PLC
    Implement function readDiscreteInputs()

--- api/coils.go
+++ api/coils.go
@@ -1,12 +1,33 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/hjhussaini/plc/models"
+)
 
 func (plc *PLC) readDiscreteInputs(
 	response http.ResponseWriter,
 	request *http.Request,
 ) {
+	block := &models.Block{}
+	err := models.ReadJSON(request.Body, &block)
+	if err != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		models.WriteJSON(Result{Message: err.Error()}, response)
 
+		return
+	}
+
+	data, err := plc.client.ReadDiscreteInputs(block.Address, block.Quantity)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		models.WriteJSON(Result{Message: err.Error()}, response)
+
+		return
+	}
+
+	models.WriteJSON(Result{Data: data}, response)
 }
 
 func (plc *PLC) readCoils(
