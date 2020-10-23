ae855eb838aeb06d628a69cd2248b135d7051e8c
Hamed Haji Hussaini <hajihussaini@afrabon.com>
2020-10-23 19:32:15 +0330

    Implement function ResponseOK()

--- api/response.go
+++ api/response.go
@@ -10,3 +10,18 @@ func ResponseError(response http.ResponseWriter, status int, message string) {
 	response.WriteHeader(status)
 	models.WriteJSON(Result{Message: message}, response)
 }
+
+func ResponseOK(
+	response http.ResponseWriter,
+	results []byte,
+	is16Bits bool,
+) {
+	data := make([]uint16, len(results))
+	for index, _ := range results {
+		if is16Bits {
+		} else {
+			data = append(data, uint16(results[index]))
+		}
+	}
+	models.WriteJSON(Result{Message: "OK", Data: data}, response)
+}
