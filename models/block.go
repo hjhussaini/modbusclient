0132f1fd80a72db2dc09d6fc118e238e6b640cb5
Hamed Haji Hussaini <hajihussaini@afrabon.com>
2020-10-23 20:10:00 +0330

    models.Block
    Add variable 'Value'

--- models/block.go
+++ models/block.go
@@ -1,6 +1,7 @@
 package models
 
 type Block struct {
-	Address  uint16 `json:"address"`
-	Quantity uint16 `json:"quantity"`
+	Address  uint16   `json:"address"`
+	Quantity uint16   `json:"quantity"`
+	Value    []uint16 `json:"value,omitempty"`
 }
